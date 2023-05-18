9e0dfecd264bb6fae400b3df077f0b2b86c36bf2
Fredele20 <victorisholaoladele@gmail.com>
2023-05-18 13:13:43 +0100

    linked list part 2

--- linked-list/main.go
+++ linked-list/main.go
@@ -20,6 +20,41 @@ func (l *LinkedList) prepend(node *Node) {
 	l.length++
 }
 
+func (l LinkedList) printListData() {
+	toPrint := l.head
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.data)
+		toPrint = toPrint.next
+		l.length--
+	}
+
+	fmt.Printf("\n")
+}
+
+func (l *LinkedList) deleteWithValue(value int) {
+	if l.length == 0 {
+		fmt.Println("There are no list in the linked list")
+		return
+	}
+
+	if l.head.data == value {
+		l.head = l.head.next
+		l.length--
+		return
+	}
+
+	previousToDelete := l.head
+	for previousToDelete.next.data != value {
+		if previousToDelete.next.next == nil {
+			return
+		}
+		previousToDelete = previousToDelete.next
+	}
+	previousToDelete.next = previousToDelete.next.next
+	l.length--
+	
+}
+
 
 func main() {
 	myList := LinkedList{}
@@ -27,11 +62,25 @@ func main() {
 	node1 := &Node{data: 48}
 	node2 := &Node{data: 18}
 	node3 := &Node{data: 16}
+	node4 := &Node{data: 25}
+	node5 := &Node{data: 20}
+	node6 := &Node{data: 2}
+
 
 	myList.prepend(node1)
 	myList.prepend(node2)
 	myList.prepend(node3)
+	myList.prepend(node4)
+	myList.prepend(node5)
+	myList.prepend(node6)
 
-	fmt.Println(myList)
+	// fmt.Println(myList)
+	myList.printListData()
+	myList.deleteWithValue(100)
+	myList.printListData()
+	myList.deleteWithValue(2)
+	myList.printListData()
+	emptyList := LinkedList{}
+	emptyList.printListData()
 
 }
