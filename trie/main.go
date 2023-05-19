c18e076a66f26b33a37d537168cb25a478fdbe43
Fredele20 <victorisholaoladele@gmail.com>
2023-05-19 10:48:12 +0100

    trie data structure implementation

---
+++ trie/main.go
@@ -0,0 +1,81 @@
+package main
+
+import "fmt"
+
+// AlphabetSize is the number of possible characters in the trie
+const AlphabetSize = 26
+
+// Node represents each node the trie
+type Node struct {
+	children [AlphabetSize]*Node
+	isEnd bool
+}
+
+// Trie represents a trie and has a pointer to the root node
+type Trie struct {
+	root *Node
+}
+
+// InitTrie will create new Trie
+func InitTrie() *Trie {
+	result := &Trie{root: &Node{}}
+	return result
+}
+
+// Insert will take in a word and add it to the trie
+func (t *Trie) Insert(w string) {
+	wordLength := len(w)
+	currentNode := t.root
+	
+	for i := 0; i < wordLength; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			currentNode.children[charIndex] = &Node{}
+		}
+		currentNode = currentNode.children[charIndex]
+	}
+	currentNode.isEnd = true
+}
+
+// Search will take in a word and RETURN true if that word is included in the trie
+func (t *Trie) Search(w string) bool {
+	wordLength := len(w)
+	currentNode := t.root
+	for i := 0; i < wordLength; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			return false
+		}
+		currentNode = currentNode.children[charIndex]
+	}
+	if currentNode.isEnd == true {
+		return true
+	}
+
+	return false
+}
+
+func main() {
+	myTrie := InitTrie()
+
+	toAdd := []string{
+		"aragorn",
+		"amazing",
+		"amazon",
+		"bukky",
+		"comfort",
+		"victor",
+		"rice",
+		"respectful",
+		"respected",
+		"original",
+	}
+
+
+	for _, v := range toAdd {
+		myTrie.Insert(v)
+	}
+
+	fmt.Println(myTrie.root)
+	fmt.Println(myTrie.Search("nice"))
+}
